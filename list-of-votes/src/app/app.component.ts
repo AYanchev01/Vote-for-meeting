1288b18bc383b069e6510a14b928389497920b85
Allie251B <ality.bamb@hotmail.com>
2023-05-07 21:58:32 +0300

    added a basic list of dates with hardcoded data

--- list-of-votes/src/app/app.component.ts
+++ list-of-votes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'list-of-votes';
+  title = 'Избрани часове и брой гласове';
 }
