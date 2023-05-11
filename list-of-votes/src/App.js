f54b036384b64aa74c21bf74ce9773539291e7a0
Allie251B <ality.bamb@hotmail.com>
2023-05-11 20:06:49 +0300

    creating an array with objects with state hook

--- list-of-votes/src/App.js
+++ list-of-votes/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <div class="titleBox">
-        <h1>Списък с дати и часове и гласове за тях</h1>
-      </div>
-    </div>
-  );
+
+  const [dates,setDates]=useState([
+    {"id":1,"date": "12.05.2022", "time": "18:30","votes": 6},
+    {"id":2,"date": "12.05.2022","time": "19:30","votes": 0},
+    {"id":3,"date": "13.05.2022","time": "18:30","votes": 1},
+    {"id":4,"date": "13.05.2022","time": "19:30","votes": 4},
+    {"id":5,"date": "14.05.2022","time": "18:30","votes": 5},
+    {"id":6,"date": "14.05.2022","time": "19:30","votes": 11},
+    {"id":7,"date": "15.05.2022","time": "18:30","votes": 1},
+    {"id":8,"date": "15.05.2022","time": "19:30","votes": 2},
+    {"id":9,"date": "16.05.2022","time": "18:30","votes": 5},
+    {"id":10,"date": "16.05.2022","time": "19:30","votes": 0},
+    {"id":11,"date": "17.05.2022","time": "18:30","votes": 1},
+    {"id":12,"date": "17.05.2022","time": "19:30","votes": 3}
+  ]);
 }
 
 export default App;
