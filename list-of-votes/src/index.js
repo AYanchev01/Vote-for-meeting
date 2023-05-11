c6c9ed05d83f3e23a97e1d6eb6b939e7827b5e15
Allie251B <ality.bamb@hotmail.com>
2023-05-11 18:40:46 +0300

    update

--- list-of-votes/src/index.js
+++ list-of-votes/src/index.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = 
-<div class="box">
-  <h1>Списък с дати и часове<br></br></h1>
-  <h3>и гласове за тях</h3>
-</div>;
-root.render(element);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+reportWebVitals();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+// const dates=[
+//   {
+//     "date": "12.05.2022",
+//     "time": "18:30",
+//     "votes": 6
+//   },
+//   {
+//     "date": "12.05.2022",
+//     "time": "18:30",
+//     "votes": 0
+//   },
+//   {
+//     "date": "12.05.2022",
+//     "time": "18:30",
+//     "votes": 9
+//   }
+// ];
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const titleElement = 
+// <div class="titleBox">
+//   <h1>Списък с дати и часове</h1>
+//   <h3>и гласове за тях</h3>
+// </div>;
+
+/* <div class="dates">
+{dates.map(line => (
+        <div key={i} style={{ margin: '30px' }}>
+          <div>{`Date: ${line.date}`}</div>
+          <div>{`Time: ${line.time}`}</div>
+          <div>{`Votes: ${line.votes}`}</div>
+        </div>
+      ))}
+</div> */
+//root.render(titleElement);
