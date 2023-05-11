5071118d558b2d4c56dd2d60d7052bd6f5dd66b8
Allie251B <ality.bamb@hotmail.com>
2023-05-11 20:54:45 +0300

    Expanded list and added title

--- list-of-votes/src/index.js
+++ list-of-votes/src/index.js
@@ -13,39 +13,3 @@ root.render(
 );
 
 reportWebVitals();
-
-// const dates=[
-//   {
-//     "date": "12.05.2022",
-//     "time": "18:30",
-//     "votes": 6
-//   },
-//   {
-//     "date": "12.05.2022",
-//     "time": "18:30",
-//     "votes": 0
-//   },
-//   {
-//     "date": "12.05.2022",
-//     "time": "18:30",
-//     "votes": 9
-//   }
-// ];
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// const titleElement = 
-// <div class="titleBox">
-//   <h1>Списък с дати и часове</h1>
-//   <h3>и гласове за тях</h3>
-// </div>;
-
-/* <div class="dates">
-{dates.map(line => (
-        <div key={i} style={{ margin: '30px' }}>
-          <div>{`Date: ${line.date}`}</div>
-          <div>{`Time: ${line.time}`}</div>
-          <div>{`Votes: ${line.votes}`}</div>
-        </div>
-      ))}
-</div> */
-//root.render(titleElement);
