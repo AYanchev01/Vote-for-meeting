c4995886c29b78be0f46a2f9de03db9cad61a781
Maria <mimi4_geri@abv.bg>
2023-05-15 15:57:27 +0300

    Successfully made login and register.

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,24 +8,27 @@ export const Login = (props) => {
         event.preventDefault();
         var users = JSON.parse(localStorage.getItem('users')) || {};
         var user = users[email];
-        if (!user || user.password !== pass) {
-            console.log(user.password);
+        
+        if (!user || user.pass !== pass) {
+            console.log(localStorage);
             alert('Invalid email or password');
-            return;
-          }
+        }else{
+            alert('Login successful!');
+        }
     }
 
     return(
-        <div className='auth-form'>
-            <form className='login-form' onSubmit={handleSubmit}>
+        <form className='login-form' onSubmit={handleSubmit}>
+            <label className='form-header'>Sign in</label>
+            <div className='box'>
                 <label htmlFor='email'>Email</label>
                 <input value={email} onChange={(event) => setEmail(event.target.value)} type='email' placeholder='yourEmail@mail.com' id='email' name='email'/>
-
+                
                 <label htmlFor='password'>Password</label>
                 <input value={pass} onChange={(event) => setPass(event.target.value)} type='password' placeholder='********' id='password' name='password'/>
-                <button type='Submit'>Log in!</button>
-            </form>
-            <button className='link-btn' onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
+            </div>
+            <button className='submit' type='Submit'>Log in!</button>
+            <button className='link-btn' onClick={() => props.onFormSwitch('Register')}>Don't have an account? Register</button>
+        </form>
     )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -18,30 +18,27 @@ export const Register = (props) => {
         else{
             users[email] = { pass };
             localStorage.setItem('users', JSON.stringify(users));
+            console.log(localStorage);
             alert('Registration sucessful!');
         }
     }
 
 
     return(
-        <div className='auth-form'>
         <form className='register-form' onSubmit={handleSubmit}>
-            <label htmlFor='email'>Email</label>
-            <input value={email} onChange={(event) => setEmail(event.target.value)} type='email' placeholder='yourEmail@mail.com' id='email' name='email'/>
-            <span className='err'></span>
+            <label className='form-header'>Register</label>
+            <div className='box'>
+                <label htmlFor='email'>Email</label>
+                <input value={email} onChange={(event) => setEmail(event.target.value)} type='email' placeholder='yourEmail@mail.com' id='email' name='email'/>
 
-            <label htmlFor='password'>Password</label>
-            <input value={pass} onChange={(event) => setPass(event.target.value)} type='password' placeholder='********' id='password' name='password'/>
-            <span className='err'></span>
-            
-            <label htmlFor='password'>Confirm password</label>
-            <input value={confirmPass} onChange={(event) => setConfirmPass(event.target.value)} type='password' placeholder='********' id='password' name='password'/>
-            <span className='err'></span>
-            
-
-            <button type='Submit'>Register!</button>
+                <label htmlFor='password'>Password</label>
+                <input value={pass} onChange={(event) => setPass(event.target.value)} type='password' placeholder='********' id='password' name='password'/>
+                
+                <label htmlFor='password'>Confirm password</label>
+                <input value={confirmPass} onChange={(event) => setConfirmPass(event.target.value)} type='password' placeholder='********' id='password' name='password'/>
+            </div>
+            <button className ='submit' type='Submit'>Register!</button>
+            <button className='link-btn' onClick={() => props.onFormSwitch('login')}>Already have an account? Log in</button>
         </form>
-        <button className='link-btn' onClick={() => props.onFormSwitch('login')}>Already have an account? Log in</button>
-    </div>
     )
 }
