96c262aa9048dff0b99901362ef66c0e5fc9e12c
Anton Yanchev <ayanchev01@gmail.com>
2023-06-19 16:14:15 +0300

    docker-compose up works

---
+++ server/run-migrations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+
+# Wait for PostgreSQL
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - running migrations"
+
+# Run migrations
+npx prisma migrate deploy
+
+npm start
